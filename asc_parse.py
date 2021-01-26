Working on scaling fixes, and adding command line args.

asc_parse.py
@@ -18,21 +18,7 @@ def load_asc(file_name: str):
     data = cv2.flip(data, 0)
     return data
 
-
-def gen_stl_from_asc(file_name: str):
-    data = load_asc(file_name)
-
-    stlgenerator.generate_from_heightmap_array(
-        data,
-        destination=file_name.rstrip('.asc')+'.stl',
-        hsep=0,
-        sep_dep=0,
-        tab_size=0,
-        tab_dep=0,
-        anchorsize=0,
-    )
-
-def gen_stls_from_ascs(list_of_asc: list, list_of_files: list):
+def gen_stls_from_ascs(list_of_asc: list, list_of_files: list, scale_adjustment = 1.0, vscale = 1.0, base = 0.0):
     # This is a bit inefficient IMO, but it's important in order
     # to ensure that we generate all stl files with a uniform height
     list_of_depth_maps = [load_asc(x) for x in list_of_asc]
@@ -61,15 +47,12 @@ def gen_stls_from_ascs(list_of_asc: list, list_of_files: list):
             tab_dep=0,
             anchorsize=0,
             hmin=lowest_value,
+            vsize=1/np.sqrt(scale_adjustment+0.5),
         )
 
 
 def main():
-    gen_stl_from_asc(sys.argv[1])
     return
 
-
 if __name__ == "__main__":
-    #sys.argv.append("test2.asc")
-    sys.argv.append("ASC\\USGS_LPC_UT_WasatchFault_L3_2013_12TVK4400055000_LAS_2016.asc")
     main()

stltools/stlgenerator.py
@@ -41,11 +41,11 @@ def generate_from_heightmap_array(heightmap, destination, hsize=1, vsize=1, base
 
     hmin = hmin or min([hm.min() for hm in heightmap])
     hmax = hmax or max([hm.max() for hm in heightmap])
-
+    vsize /= 750
     heightmap       -= hmin                                              #Set base elevation to 0
-    heightmap       *= vsize/hmax                                        #Convert heightmap from input units to output units
+    heightmap       *= vsize#/hmax                                        #Convert heightmap from input units to output units
     heightmap       += base                                              #Add the indicated amount of base (in output units)
-    h_scale          = hsize/heightmap[0].shape[1]                       #Find the horizontal scale
+    h_scale = hsize/min((heightmap[0].shape[1], heightmap[0].shape[0]))  #Find the horizontal scale
     tab_size         = math.ceil(tab_size/h_scale)                       #Convert tab size from output units to cells
     separation_array = np.zeros(shape=(1,heightmap[0].shape[1]))+sep_dep #Separation array equal to width of piece set to sep_dep height
     separation_array[0,0:tab_size] = tab_dep                             #Add tabs